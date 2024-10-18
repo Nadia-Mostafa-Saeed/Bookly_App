a07b244d51cb0d18b23d9b80a6c321c19c84146c
nadiamostafa <nadiamostafasaeed@gmail.com>
2024-10-18 23:29:50 +0300

    best seller list view part 1

--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/Features/home/presentation/views/widgets/best_seller_list_view_item.dart';
 import 'package:bookly/Features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly/Features/home/presentation/views/widgets/featured_books_list_view.dart';
 import 'package:bookly/core/utils/styles.dart';
@@ -22,6 +23,7 @@ class HomeViewBody extends StatelessWidget {
             'Best Seller',
             style: Styles.textMedium,
           ),
+          BestSellerListViewItem(),
         ],
       ),
     );
