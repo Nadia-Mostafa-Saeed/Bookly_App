c8cec99acc289fa52ae42b6adaebd95a7e961b8a
nadiamostafa <nadiamostafasaeed@gmail.com>
2024-10-20 23:25:35 +0300

    add title & sub title to book details

--- lib/Features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/Features/home/presentation/views/widgets/book_details_view_body.dart
@@ -15,8 +15,23 @@ class BookDetailsViewBody extends StatelessWidget {
         children: [
           const CustomAppBarBookDetails(),
           Padding(
-            padding: EdgeInsets.symmetric(horizontal: width * .17),
+            padding: EdgeInsets.symmetric(horizontal: width * .2),
             child: const CustomBookImage(),
+          ),
+          const SizedBox(
+            height: 43,
+          ),
+          Text(
+            'The Jungle Book',
+            style: Styles.textStyle30.copyWith(fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(
+            height: 6,
+          ),
+          Text(
+            'Rudyard Kipling',
+            style: Styles.textStyle18.copyWith(
+                fontStyle: FontStyle.italic, fontWeight: FontWeight.w500),
           ),
         ],
       ),
