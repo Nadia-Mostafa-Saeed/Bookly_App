0bd7f7286de657ae27a3c90127e0a225e7d193df
nadiamostafa <nadiamostafasaeed@gmail.com>
2025-04-09 02:27:52 +0200

    register book entity adapter and open featured box

--- lib/core/utils/constants.dart
+++ lib/core/utils/constants.dart
@@ -4,3 +4,4 @@ const kPrimaryColor = Color(0xff100B20);
 const kTransitionDuration = Duration(microseconds: 250);
 const kGtSectraFine = 'Gt Sectra Fine';
 const kPadding = EdgeInsets.symmetric(horizontal: 30);
+const kFeaturedBox = 'featured_box';
